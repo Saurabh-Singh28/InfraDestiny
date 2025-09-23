8fc59748bd73644d438285f3a2d503dfba0d1d1b
Saurabh Singh <prosaurabhboy@gmail.com>
2025-09-23 13:53:09 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { Toaster } from "./components/ui/toaster"  // adjust relative path
-
+import { Toaster } from "@/components/ui/toaster"
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
